fac03997c1099999f2a3f810284bfce03271b2a5
Ubuntu <azureuser@lulu.vcncxl2aizxebgscxxxbtkmbvg.hx.internal.cloudapp.net>
2022-08-21 16:42:31 +0000

    Revise some document comments.

--- algorithm/stock_predictor/data_handler.py
+++ algorithm/stock_predictor/data_handler.py
@@ -4,8 +4,7 @@ class Alpha158TwoWeeks(Alpha158):
     
     """
     Override the get_label_config method in Alpha158.
-    This will build a data handler that labels the items in different way.
-    Here we labels the item with the price increase within next 10 days.
+    This will build a data handler that labels the items with price increase in next 10 trading days.
     """
     def get_label_config(self):
         return (["Ref($close, -10)/Ref($close, -1) - 1"], ["LABEL0"])
