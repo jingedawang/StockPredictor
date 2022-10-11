e848dd66b9c01128f04a86c979ae5201b717e120
Yifan Zou <yifanzou@microsoft.com>
2022-10-11 23:55:21 +0800

    fix: adjust label of arkArea

--- frontend/src/component/Query.js
+++ frontend/src/component/Query.js
@@ -99,6 +99,7 @@ export function Query() {
               smooth: true,
               data: props.data.map((i) => i.value),
               markArea: {
+                silent: true,
                 itemStyle: {
                   color: "rgba(255, 173, 177, 0.4)",
                 },
@@ -107,6 +108,9 @@ export function Query() {
                     {
                       name: "Forecast after Two Weeks",
                       xAxis: props.data[length - 2].date,
+                      label: {
+                        position: 'left',
+                      },
                     },
                     {
                       xAxis: props.data[length - 1].date,
