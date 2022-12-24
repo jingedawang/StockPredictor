6223a6cdd6112d2f55c59fad08b941cc7d06fe37
Jinge Wang <jingewang@microsoft.com>
2022-12-24 21:40:03 +0800

    Fix missing data.

--- stock_predictor/tools.py
+++ stock_predictor/tools.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     """
     # Read params from command line.
     argparser = argparse.ArgumentParser(description='Tools of stock predictor.')
-    argparser.add_argument('name', type=str, help='The name of the requested tool. Choose from [predict_all], [fix_missing_prediction] and [update_stock_list].')
+    argparser.add_argument('name', type=str, help='The name of the requested tool. Choose from [predict_all], [update_stock_list], [fix_missing_data] and [fix_missing_prediction].')
     args = argparser.parse_args()
 
     service = Service()
@@ -18,11 +18,14 @@ if __name__ == '__main__':
     if args.name == 'predict_all':
         # Do today's prediction for all stocks.
         service.predict_all(date=datetime.date.today().strftime('%Y-%m-%d'))
-    elif args.name == 'fix_missing_prediction':
-        # Fix the missing predictions by re-try predicting for the missed dates.
-        service.fix_mising_prediction()
     elif args.name == 'update_stock_list':
         # Update stock list according to official stock exchange website.
         service.load_stock_list()
+    elif args.name == 'fix_missing_data':
+        # Fix the missing data by re-downloading them.
+        service.fix_missing_data()
+    elif args.name == 'fix_missing_prediction':
+        # Fix the missing predictions by re-try predicting for the missed dates.
+        service.fix_mising_prediction()
     else:
         print(f'Not supported tool name: {args.name}')
